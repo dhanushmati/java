1cf11d6a70cbaa6136e2cbe61a4367e9b98c1ed1
Dhanushmati S <91077806+dhanushmati@users.noreply.github.com>
2021-11-29 14:55:44 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+/* PRINT FIBONACCI SERIES */
+
+class Test{
+public static void main(String[] args){
+int a=0,b=1,c;
+System.out.print(a+" "+b);
+for(int i=1;1<=10;i++){
+c=a+b;
+Sysetm.out.print(" "+c);
+a=b;
+b=c;
+}
+}
+} 
